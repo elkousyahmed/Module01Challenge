ba4c981205ce6c3d6840827e6a9d166dcff83f5c
DELL <DELL@DESKTOP-D49L819>
2024-02-27 07:34:38 +0200

    Add project files.

---
+++ Module01Challenge/Module01Challenge.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+
+namespace Module01Challenge
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Module01Challenge : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+
+            var uidoc = commandData.Application.ActiveUIDocument;
+            var doc = uidoc.Document;
+
+
+
+
+            Transaction t = new Transaction(doc);
+
+            t.Start("do");
+
+            for (var i = 1; i < 250; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Level lv = Level.Create(doc, i);
+                    lv.Name = $"FizzBuzz {i}";
+                   
+                }
+                else if (i % 3 == 0)
+                {
+                    Level lv = Level.Create(doc, i);
+                    lv.Name = $"Fizz {i}";
+                    
+                }
+                else if (i % 5 == 0)
+                {
+                    Level lv = Level.Create(doc, i);
+                    lv.Name = $"Buzz {i}";
+                   
+
+                }
+            }
+
+
+
+            t.Commit();
+
+            return Result.Succeeded;
+
+
+        }
+    }
+}
